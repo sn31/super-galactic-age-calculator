407091ea50f0037b9045296434bcae67e3a16044
SkyeN <skye@dames.es>
2018-10-12 13:15:02 -0700

    remove monthObj

--- src/galactic-logic.js
+++ src/galactic-logic.js
@@ -6,21 +6,6 @@ const planetValues = {
   Jupiter: 11.86
 };
 
-const monthObj = {
-    1: 31,
-    2: 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31
-  };
-
 export class UserAge {
   constructor(userDOB, userLifeExpectancy) {
     this.DOB = new Date(userDOB);
