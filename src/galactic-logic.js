f123e288955a4a38e2079da1f1ad0e105191d897
SkyeN <skye@dames.es>
2018-10-12 09:48:46 -0700

    find the age of the user in second before converting it to years to maximize the accurate of the calculations

--- src/galactic-logic.js
+++ src/galactic-logic.js
@@ -14,42 +14,43 @@ export class UserAge{
     
     calculateEarthAgeInYears()
     {
-        this.userAgeInYears = todayDate.getFullYear()-this.DOB.getFullYear();
+        let userAgeInSecond = (todayDate.getTime() - this.DOB.getTime())/1000;
+        this.userAgeInYears = (userAgeInSecond/31622400).toFixed(1);
         return this.userAgeInYears;
     }
     calculateMercuryAge() {
-        this.mercuryAge = (this.userAgeInYears/0.24).toFixed(2);
+        this.mercuryAge = (this.userAgeInYears/0.24).toFixed(1);
         return this.mercuryAge;
     }
     calculateVenusAge() {
-        this.venusAge = (this.userAgeInYears/0.62).toFixed(2);
+        this.venusAge = (this.userAgeInYears/0.62).toFixed(1);
         return this.venusAge;
     }
     calculateMarchAge() {
-       this.marchAge = (this.userAgeInYears/1.88).toFixed(2);
+       this.marchAge = (this.userAgeInYears/1.88).toFixed(1);
         return this.marchAge;
     }
     calculateJupiterAge() {
-        this.jupiterAge = (this.userAgeInYears/11.86).toFixed(2);
+        this.jupiterAge = (this.userAgeInYears/11.86).toFixed(1);
         return this.jupiterAge;
     }
     yearsLeftMercury(){
-        let userLifeExpectancyMercury = (this.userLifeExpectancy/0.24).toFixed(2);
+        let userLifeExpectancyMercury = (this.userLifeExpectancy/0.24).toFixed(1);
         let yearsLeftMercury = userLifeExpectancyMercury - this.mercuryAge;
         return yearsLeftMercury;
     }
     yearsLeftVenus(){
-        let userLifeExpectancyVenus = (this.userLifeExpectancy/0.62).toFixed(2);
+        let userLifeExpectancyVenus = (this.userLifeExpectancy/0.62).toFixed(1);
         let yearsLeftVenus = userLifeExpectancyVenus - this.venusAge;
         return yearsLeftVenus;
     }
     yearsLeftMarch(){
-        let userLifeExpectancyMarch = (this.userLifeExpectancy/1.88).toFixed(2);
+        let userLifeExpectancyMarch = (this.userLifeExpectancy/1.88).toFixed(1);
         let yearsLeftMarch = userLifeExpectancyMarch - this.marchAge;
         return yearsLeftMarch;
     }
     yearsLeftJupiter(){
-        let userLifeExpectancyJupiter = (this.userLifeExpectancy/11.86).toFixed(2);
+        let userLifeExpectancyJupiter = (this.userLifeExpectancy/11.86).toFixed(1);
         let yearsLeftJupiter = userLifeExpectancyJupiter - this.jupiterAge;
         return yearsLeftJupiter;
     }
