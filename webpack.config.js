7606e9b6e00fa63c8c45bc179bc6de60cd2061f8
SkyeN <skye@dames.es>
2018-10-12 13:52:30 -0700

    disable url loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader?url=false'
         ]
       },
       {                     
