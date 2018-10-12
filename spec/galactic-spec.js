9ab12c7b607257a6a4b0ad987c1d563729e8d188
SkyeN <skye@dames.es>
2018-10-12 10:12:27 -0700

    add tests for life expectancy methods

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -2,7 +2,9 @@ import { UserAge } from "../src/galactic-logic";
 
 describe("Galactic Age Calculator", function() {
   let userDOB = "March 31, 1994";
-  let newUser = new UserAge(userDOB);
+  let userLifeExpectancy = 80;
+  let newUser = new UserAge(userDOB, userLifeExpectancy);
+
   it("should correctly return the user's age in Earth years", function() {
     expect(newUser.calculateEarthAgeInYears()).toEqual("24.5");
   });
@@ -18,11 +20,23 @@ describe("Galactic Age Calculator", function() {
   it("should correctly return the user's age in Jupiter years.", function() {
     expect(newUser.calculateJupiterAge()).toEqual("2.1");
   });
+  it("should correctly return the number of remaining years the user have left on Earth", function() {
+    expect(newUser.yearsLeftEarth()).toEqual(55.5);
+  });
+  it("should correctly return the number of remaining years the user have left on Mercury", function() {
+    expect(newUser.yearsLeftMercury()).toEqual("231.2");
+  });
+  it("should correctly return the number of remaining years the user have left on Venus", function() {
+    expect(newUser.yearsLeftVenus()).toEqual("89.5");
+  });
+  it("should correctly return the number of remaining years the user have left on March", function() {
+    expect(newUser.yearsLeftMarch()).toEqual("29.6");
+  });
+  it("should correctly return the number of remaining years the user have left on Jupiter", function() {
+    expect(newUser.yearsLeftJupiter()).toEqual("4.6");
+  });
 });
 
 
-
-// * The program will return the user's age in Jupiter years. (A Jupiter year is 11.86 Earth years.)
-// * The program will calculate the user’s life expectancy using average life expectancy for their demographic.
 // * The program will determines how many years a user has left to live on each planet.
 // * If a user has already surpassed the average life expectancy, the program will return the number of years they have lived past the life expectancy.
