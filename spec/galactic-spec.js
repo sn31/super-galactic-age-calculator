0c79832c78e159d7d883987ee161b83a413fa202
SkyeN <skye@dames.es>
2018-10-12 11:55:07 -0700

    consolidate methods and adjust tests

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -9,33 +9,44 @@ describe("Galactic Age Calculator", function() {
     expect(newUser.calculateEarthAgeInYears()).toEqual("24.5");
   });
   it("should correctly return the user's age in Mercury years.", function() {
-    expect(newUser.calculateMercuryAge()).toEqual("102.1");
+    expect(newUser.calculateAgeOnPlanet("Mercury")).toEqual("102.1");
   });
   it("should correctly return the user's age in Venus years.", function() {
-    expect(newUser.calculateVenusAge()).toEqual("39.5");
+    expect(newUser.calculateAgeOnPlanet("Venus")).toEqual("39.5");
   });
   it("should correctly return the user's age in Mars years.", function() {
-    expect(newUser.calculateMarsAge()).toEqual("13.0");
+    expect(newUser.calculateAgeOnPlanet("Mars")).toEqual("13.0");
   });
   it("should correctly return the user's age in Jupiter years.", function() {
-    expect(newUser.calculateJupiterAge()).toEqual("2.1");
+    expect(newUser.calculateAgeOnPlanet("Jupiter")).toEqual("2.1");
   });
   it("should correctly return the number of remaining years the user have left on Earth", function() {
-    expect(newUser.yearsLeftOnEarth()).toEqual("You still have 55.5 years of life expectancy left on Earth!");
+    let newUser = new UserAge("March 31, 1904", 80);
+    newUser.calculateEarthAgeInYears();
+    expect(newUser.calculateYearsLeftOnPlanet("Earth")).toEqual(
+      "You have lived 34.3 years passed the life expectancy on Earth!"
+    );
   });
   it("should correctly return the number of remaining years the user have left on Mercury", function() {
-    expect(newUser.yearsLeftOnMercury()).toEqual("231.2");
+    expect(newUser.calculateYearsLeftOnPlanet("Mercury")).toEqual(
+      "You still have 231.2 years of life expectancy left on Mercury!"
+    );
   });
   it("should correctly return the number of remaining years the user have left on Venus", function() {
-    expect(newUser.yearsLeftOnVenus()).toEqual("89.5");
+    expect(newUser.calculateYearsLeftOnPlanet("Venus")).toEqual(
+      "You still have 89.5 years of life expectancy left on Venus!"
+    );
   });
   it("should correctly return the number of remaining years the user have left on March", function() {
-    expect(newUser.yearsLeftOnMars()).toEqual("29.6");
+    expect(newUser.calculateYearsLeftOnPlanet("Mars")).toEqual(
+      "You still have 29.6 years of life expectancy left on Mars!"
+    );
   });
   it("should correctly return the number of remaining years the user have left on Jupiter", function() {
-    expect(newUser.yearsLeftOnJupiter()).toEqual("4.6");
+    expect(newUser.calculateYearsLeftOnPlanet("Jupiter")).toEqual(
+      "You still have 4.6 years of life expectancy left on Jupiter!"
+    );
   });
 });
-
 
 // * If a user has already surpassed the average life expectancy, the program will return the number of years they have lived past the life expectancy.
--- src/galactic-logic.js
+++ src/galactic-logic.js
@@ -1,68 +1,41 @@
 let todayDate = new Date();
+const planetValues = {
+  Mercury: 0.24,
+  Venus: 0.62,
+  Mars: 1.88,
+  Jupiter: 11.86
+};
 
-export class UserAge{
-    constructor (userDOB, userLifeExpectancy)
-    {
-        this.DOB = new Date(userDOB);
-        this.userAgeInYears;
-        this.userLifeExpectancy = userLifeExpectancy;
-        this.mercuryAge;
-        this.venusAge;
-        this.marsAge;
-        this.jupiterAge;
-    }
-    
-    calculateEarthAgeInYears()
-    {
-        let userAgeInSecond = (todayDate.getTime() - this.DOB.getTime())/1000;
-        this.userAgeInYears = (userAgeInSecond/31622400).toFixed(1);
-        return this.userAgeInYears;
-    }
-    calculateMercuryAge() {
-        this.mercuryAge = (this.userAgeInYears/0.24).toFixed(1);
-        return this.mercuryAge;
-    }
-    calculateVenusAge() {
-        this.venusAge = (this.userAgeInYears/0.62).toFixed(1);
-        return this.venusAge;
-    }
-    calculateMarsAge() {
-       this.marsAge = (this.userAgeInYears/1.88).toFixed(1);
-        return this.marsAge;
-    }
-    calculateJupiterAge() {
-        this.jupiterAge = (this.userAgeInYears/11.86).toFixed(1);
-        return this.jupiterAge;
-    }
-    yearsLeftOnEarth()
-    {
-        let yearsLeftOnEarth = this.userLifeExpectancy - this.userAgeInYears;
-        let result = `You still have ${yearsLeftOnEarth} years of life expectancy left on Earth!`;
-        if (yearsLeftOnEarth < 0)
-        {
-            result = `You have live ${yearsLeftOnEarth} years passed the life expectancy on Earth!`;
-        }
-        return result;
-    }
-    yearsLeftOnMercury(){
-        let userLifeExpectancyMercury = (this.userLifeExpectancy/0.24);
-        let yearsLeftOnMercury = (userLifeExpectancyMercury - parseFloat(this.mercuryAge)).toFixed(1);
-        return yearsLeftOnMercury;
-    }
-    yearsLeftOnVenus(){
-        let userLifeExpectancyVenus = (this.userLifeExpectancy/0.62);
-        let yearsLeftOnVenus = (userLifeExpectancyVenus - parseFloat(this.venusAge)).toFixed(1);
-        return yearsLeftOnVenus;
-    }
-    yearsLeftOnMars(){
-        let userLifeExpectancyMars = (this.userLifeExpectancy/1.88).toFixed(1);
-        let yearsLeftOnMars = (userLifeExpectancyMars - parseFloat(this.marsAge)).toFixed(1);        
-        return yearsLeftOnMars;
-    }
-    yearsLeftOnJupiter(){
-        let userLifeExpectancyJupiter = (this.userLifeExpectancy/11.86).toFixed(1);
-        let yearsLeftOnJupiter = (userLifeExpectancyJupiter - parseFloat(this.jupiterAge)).toFixed(1);
-        return yearsLeftOnJupiter;
-    }
+export class UserAge {
+  constructor(userDOB, userLifeExpectancy) {
+    this.DOB = new Date(userDOB);
+    this.userAgeInYears;
+    this.userLifeExpectancy = userLifeExpectancy;
+  }
 
+  calculateEarthAgeInYears() {
+    let userAgeInSecond = (todayDate.getTime() - this.DOB.getTime()) / 1000;
+    this.userAgeInYears = (userAgeInSecond / 31622400).toFixed(1);
+    return this.userAgeInYears;
+  }
+  calculateAgeOnPlanet(planet) {
+    let ageOnPlanet = (this.userAgeInYears / planetValues[planet]).toFixed(1);
+    return ageOnPlanet;
+  }
+  calculateYearsLeftOnPlanet(planet) {
+    let yearsLeftOnPlanet;
+    if (planet === "Earth") {
+      yearsLeftOnPlanet = this.userLifeExpectancy - this.userAgeInYears;
+    } else {
+      yearsLeftOnPlanet = (
+        this.userLifeExpectancy / planetValues[planet] -
+        parseFloat(this.calculateAgeOnPlanet(planet))
+      ).toFixed(1);
+    }
+    let result = `You still have ${yearsLeftOnPlanet} years of life expectancy left on ${planet}!`;
+    if (yearsLeftOnPlanet < 0) {
+      result = `You have lived ${Math.abs(yearsLeftOnPlanet)} years passed the life expectancy on ${planet}!`;
+    }
+    return result;
+  }
 }
