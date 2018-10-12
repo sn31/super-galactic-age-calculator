4465c5ff0c76d9d0fcbd0a4f32edf5234f484af3
SkyeN <skye@dames.es>
2018-10-12 08:15:36 -0700

    add initial configs and files

---
+++ karma.conf.js
@@ -0,0 +1,36 @@
+const webpackConfig = require('./webpack.config.js');
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'src/*.js',
+      'spec/*spec.js'
+    ],
+    webpack: webpackConfig,
+    exclude: [
+    ],
+    preprocessors: {
+      'src/*.js': ['webpack', 'sourcemap', 'coverage'],
+      'spec/*spec.js': ['webpack', 'sourcemap']
+    },
+    plugins: [
+      'karma-jquery',
+      'karma-webpack',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter',
+      'karma-sourcemap-loader',
+      'karma-coverage'
+    ],
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
